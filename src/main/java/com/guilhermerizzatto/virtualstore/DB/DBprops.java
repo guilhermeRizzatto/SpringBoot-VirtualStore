3ceda48ccb698661c9cf9e9d95d093dabfc1d8b5
Guilherme <guimarquesrz@gmail.com>
2024-03-05 22:15:19 -0300

    DB props have all the DB properties now

--- src/main/java/com/guilhermerizzatto/virtualstore/DB/DBprops.java
+++ src/main/java/com/guilhermerizzatto/virtualstore/DB/DBprops.java
@@ -1,19 +1,10 @@
 package com.guilhermerizzatto.virtualstore.DB;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 public class DBprops {
 
-	public static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("URL TO DB.PROPERTIES")) {
-			Properties props = new Properties();
-			props.load(fs);
-			return props;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+	public static final String dburl = "jdbc:postgresql://localhost:5432/virtualstore-testDB";
+	public static final String user = "postgres";
+	public static final String password = "voyager";
+
 }
